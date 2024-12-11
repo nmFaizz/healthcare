6e5169bd274248e6a0fe7cd57201521fd3c423dc
MadeSatya5 <madesatya505@gmail.com>
2024-12-11 17:34:40 +0700

    add footer

--- service.php
+++ service.php
@@ -22,6 +22,10 @@
     /* -------------- Nav ----------------- */
     a {
         text-decoration: none;
+        color: white;
+    }
+    #footer a:hover{
+        color: black;
     }
 
     .button-transparent {
@@ -52,6 +56,9 @@
     }
     /* -------------- End Nav ----------------- */
 
+    li{
+        list-style: none;
+    }
 
     #service{
         color: white;
@@ -109,11 +116,27 @@
         height: 220px;
     }
 
+    /* ---- Customer Review ---- */
     #customer-review .card{
-        width: 250px;
+        width: 300px;
     }
-    #customer-review .card img{
-        height: 170px;
+    #customer-review .card .stars{
+        width: 130px;
+        height: 22px;
+    }
+    #customer-review .card .user-profile{
+        width: 50px;
+        height: 50px;
+    }
+
+    /* ---- FAQ ---- */
+    #faq .card{
+        width: 300px;
+        height: 150px;
+    }
+    #faq .arrow{
+        width: 8px;
+        height: 17px;
     }
 
 </style>
@@ -151,52 +174,57 @@
         </div>
     </nav>
     
-    <div class="container-fluid py-5 d-flex flex-column flex-lg-row justify-content-center bg-hero" id="service">
-        <div class="p-5 lc-block col-xxl-7 col-lg-8 col-12 jumbotron-content">
-            <div class="lc-block">
-                <div editable="rich">
-                    <h2 class="fw-bolder display-3">Meet the Best <br/> Hospital</h2>
+    <!------ Appointment ------->
+    <section>
+        <div class="container-fluid py-5 d-flex flex-column flex-lg-row justify-content-center bg-hero" id="service">
+            <div class="p-5 lc-block col-xxl-7 col-lg-8 col-12 jumbotron-content">
+                <div class="lc-block">
+                    <div editable="rich">
+                        <h2 class="fw-bolder display-3">Meet the Best <br/> Hospital</h2>
+                    </div>
+                </div>
+                <div class="lc-block col-md-8 mt-5">
+                    <div editable="rich">
+                        <p class="lead fw-normal">
+                            We know how large objects will act, 
+                            but things on a small scale.
+                        </p>
+                    </div>
+                </div>
+                <div class="d-flex gap-3 mt-5">
+                    <a class="button-green text-light text-center rounded-pill fw-semibold" href="#">Get Quote Now</a>
+                    <a class="button-transparent text-light text-center rounded-pill fw-semibold border-green" href="#">Learn More</a>
                 </div>
             </div>
-            <div class="lc-block col-md-8 mt-5">
-                <div editable="rich">
-                    <p class="lead fw-normal">
-                        We know how large objects will act, 
-                        but things on a small scale.
-                    </p>
+            <div class="form-appointment d-flex flex-column text-black fw-semibold">
+                <h1>Book Appointment</h1>
+                <div class="appointment-input d-flex flex-column gap-2">
+                    <label for="name" class="form-label">Name *</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Full Name *">
+                    <label for="email" class="form-label">Email Address *</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="example@gmail.com">
+                    <label for="department" class="form-label">Department *</label>
+                    <select class="form-select form-select-sm" aria-label="Small select example" name="department" id="department">
+                        <option selected>Please Select</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                    <label for="time" class="form-label" id="time">Time *</label>
+                    <select class="form-select form-select-sm" aria-label="Small select example" name="time" id="time">
+                        <option selected>4:00 Available</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
                 </div>
-            </div>
-            <div class="d-flex gap-3 mt-5">
-                <a class="button-green text-light text-center rounded-pill fw-semibold" href="#">Get Quote Now</a>
-                <a class="button-transparent text-light text-center rounded-pill fw-semibold border-green" href="#">Learn More</a>
+                <button class="button-green text-light text-center border-0" href="#">Book Appointment</button>
             </div>
         </div>
-        <div class="form-appointment d-flex flex-column text-black fw-semibold">
-            <h1>Book Appointment</h1>
-            <div class="appointment-input d-flex flex-column gap-2">
-                <label for="name" class="form-label">Name *</label>
-                <input type="text" class="form-control" name="name" id="name" placeholder="Full Name *">
-                <label for="email" class="form-label">Email Address *</label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="example@gmail.com">
-                <label for="department" class="form-label">Department *</label>
-                <select class="form-select form-select-sm" aria-label="Small select example" name="department" id="department">
-                    <option selected>Please Select</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-                <label for="time" class="form-label" id="time">Time *</label>
-                <select class="form-select form-select-sm" aria-label="Small select example" name="time" id="time">
-                    <option selected>4:00 Available</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-            </div>
-            <button class="button-green text-light text-center border-0" href="#">Book Appointment</button>
-        </div>
-    </div>
-
+    </section>
+    <!------ End Appointment ------->
+    
+    <!------ Find Doctor ------->
     <section>
         <div class="container my-5 find-doctor bg-white rounded-4 p-5">
             <h1>Find A Doctor</h1>
@@ -217,6 +245,7 @@
             </div>
         </div>
     </section>
+    <!------ End Find Doctor ------->
     
 
     <!------- Services we Prodivide -------->
@@ -281,25 +310,171 @@
     
     <!------- Customer Reviews------->
     <section>
-    <div class="container" id="customer-review">
+    <div class="container pb-5" id="customer-review">
         <h1 style="color: #007E85" class="text-center">What Our Customer Say</h1>
         <p class="text-center mb-5">Problems trying to resolve the conflict between the two major realms of <br> Classical physics: Newtonian mechanics </p>
         <div class="row gap-5 mb-3 d-flex justify-content-center">
-            <div class="card col-sm-6 col-lg-4 pt-3 rounded-4">
-                <img src="./images/icn bxs-star.png" alt="">
-                <img src="./images/card-content.png" class="card-img-top" alt="...">
+            <div class="card col-sm-6 col-lg-4 p-4">
+                <img class="stars mb-3" src="./images/stars.png" alt="">
+                <p>Slate helps you see how many 
+                more days you need to work to 
+                reach your financial goal.</p>
+                <img class="user-profile" src="./images/user1.jpg" alt="user">
             </div>
-            <div class="card col-sm-6 col-lg-4 pt-3 rounded-4">
-                <img src="./images/card-content.png" class="card-img-top" alt="...">
-                
+            <div class="card col-sm-6 col-lg-4 p-4">
+                <img class="stars mb-3" src="./images/stars.png" alt="">
+                <p>Slate helps you see how many 
+                more days you need to work to 
+                reach your financial goal.</p>
+                <img class="user-profile" src="./images/user2.jpg" alt="user">
             </div>
-            <div class="card col-sm-6 col-lg-4 pt-3 rounded-4">
-                <img src="./images/card-content.png" class="card-img-top" alt="...">
+            <div class="card col-sm-6 col-lg-4 p-4">
+                <img class="stars mb-3" src="./images/stars.png" alt="user">
+                <p>Slate helps you see how many 
+                more days you need to work to 
+                reach your financial goal.</p>
+                <img class="user-profile" src="./images/user3.jpg" alt="user">
             </div>
         </div>
         
     </section>
+    <!------- End Customer Reviews------->
 
+    <!------- FAQ------->         
+    <section>
+        <div class="container mt-5" id="faq">
+            <h1 class="text-center">FAQ</h1>
+            <p class="text-center mb-5">Problems trying to resolve the conflict between the two major realms of <br> Classical physics: Newtonian mechanics </p>
+            <div class="row gap-5 mb-3 d-flex justify-content-center">
+                <div class="card col-sm-6 col-lg-4 p-4 d-flex flex-row ">
+                    <img class="arrow me-3" src="./images/icn arrow-right icn-xs.png" alt="">
+                    <div>
+                        <p class="fw-bold">The Quick Fox Jumps Over The 
+                        Lazy Dog</p>
+                        <p>Things on a very small scale 
+                        behave like nothing </p>  
+                    </div>
+                </div>
+                <div class="card col-sm-6 col-lg-4 p-4 d-flex flex-row">
+                    <img class="arrow me-3" src="./images/icn arrow-right icn-xs.png" alt="">
+                    <div>
+                        <p class="fw-bold">The Quick Fox Jumps Over The 
+                        Lazy Dog</p>
+                        <p>Things on a very small scale 
+                        behave like nothing </p>  
+                    </div>
+                </div>
+                <div class="card col-sm-6 col-lg-4 p-4 d-flex flex-row">
+                    <img class="arrow me-3" src="./images/icn arrow-right icn-xs.png" alt="">
+                    <div>
+                        <p class="fw-bold">The Quick Fox Jumps Over The 
+                        Lazy Dog</p>
+                        <p>Things on a very small scale 
+                        behave like nothing </p>  
+                    </div>
+                </div>
+                <div class="card col-sm-6 col-lg-4 p-4 d-flex flex-row">
+                    <img class="arrow me-3" src="./images/icn arrow-right icn-xs.png" alt="">
+                    <div>
+                        <p class="fw-bold">The Quick Fox Jumps Over The 
+                        Lazy Dog</p>
+                        <p>Things on a very small scale 
+                        behave like nothing </p>  
+                    </div>
+                </div>
+                <div class="card col-sm-6 col-lg-4 p-4 d-flex flex-row">
+                    <img class="arrow me-3" src="./images/icn arrow-right icn-xs.png" alt="">
+                    <div>
+                        <p class="fw-bold">The Quick Fox Jumps Over The 
+                        Lazy Dog</p>
+                        <p>Things on a very small scale 
+                        behave like nothing </p>  
+                    </div>
+                </div>
+                <div class="card col-sm-6 col-lg-4 p-4 d-flex flex-row">
+                    <img class="arrow me-3" src="./images/icn arrow-right icn-xs.png" alt="">
+                    <div>
+                        <p class="fw-bold">The Quick Fox Jumps Over The 
+                        Lazy Dog</p>
+                        <p>Things on a very small scale 
+                        behave like nothing </p>  
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <!------- End FAQ------->
+
+    <!------- Footer -------->
+    <section class="mt-5">
+        <div style="background-color: #007E85;" class="text-white py-4" id="footer">
+            <div class="container p-5 d-flex flex-wrap justify-content-between">
+                <div class="mb-4 me-4">
+                    <a class="navbar-brand" href="#">
+                        <img src="./images/logofooter.png" alt="logo" width="200">
+                    </a>
+                    <p>Copyright © 2022 BRIX Templates <br> | All Rights Reserved </p>
+                </div>
+                <div class="mb-4 me-4">
+                    <h5 class="fw-bold mb-4" >Product</h5>
+                    <li class="d-flex flex-column">
+                        <a href="#" >Features</a>
+                        <a href="#" >Pricing</a>
+                        <a href="#" >Case Studies</a>
+                        <a href="#" >Reviews</a>
+                        <a href="#" >Updates</a>
+                    </li>
+                </div>
+                <div class="mb-4 me-4">
+                    <h5 class="fw-bold mb-4" >Company</h5>
+                    <li class="d-flex flex-column">
+                        <a href="#" >About</a>
+                        <a href="#" >Contact Us</a>
+                        <a href="#" >Careers</a>
+                        <a href="#" >Cultures</a>
+                        <a href="#" >Blog</a>
+                    </li>
+                </div>
+                <div class="mb-4 me-4">
+                    <h5 class="fw-bold mb-4" >Support</h5>
+                    <li class="d-flex flex-column">
+                        <a href="#" >Getting Started</a>
+                        <a href="#" >Help Center</a>
+                        <a href="#" >Server Status</a>
+                        <a href="#" >Report a Bug</a>
+                        <a href="#" >Chat Support</a>
+                    </li>
+                </div>
+                <div class="mb-4 me-4">
+                    <h5 class="fw-bold mb-4" >Support</h5>
+                    <li class="d-flex flex-column">
+                        <a href="#" >
+                            <img src="./images/Facebook.png" alt="">
+                            Facebook
+                        </a>
+                        <a href="#" >
+                            <img src="./images/Twitter.png" alt="">
+                            Twitter
+                        </a>
+                        <a href="#" >
+                            <img src="./images/Instagram.png" alt="">
+                            Instagram
+                        </a>
+                        <a href="#" >
+                            <img src="./images/LinkedIn.png" alt="">
+                            LinkedIn
+                        </a>
+                        <a href="#" >
+                            <img src="./images/YouTube.png" alt="">
+                            Youtube
+                        </a>
+                    </li>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!------- End Footer -------->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
